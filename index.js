78b5cf47905fa89ad5ad1646927811d0a160ba0d
jspl151 <jayasutha.151@gmail.com>
2023-06-08 08:13:59 +0000

    Get animal count.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const animals = ["dog", "cat", "wolf", "lion", "dog", "cat", "wolf", "lion", "dog", "tiger"];
 
-let countObject = animals.reduce((acc,currentValue)=>
- {
-    return (
-        acc[currentValue] ? acc[currentValue]+1 : (acc[currentValue] = 1),
-        acc
-    );
+let countObject = animals.reduce((acc, currentValue) => {
+  return (
+    acc[currentValue] ? acc[currentValue] + 1 : (acc[currentValue] = 1),
+    acc
+  );
 },
-{});
+  {});
 console.log(countObject);
